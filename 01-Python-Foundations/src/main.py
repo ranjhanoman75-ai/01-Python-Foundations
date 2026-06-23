8a479ea15303f95df44d5d868d5cbd5fdeb90cea
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-06-23 12:48:13 +0500

    Day 1 part 2: Python Object model and mutability

---
+++ 01-Python-Foundations/src/main.py
@@ -0,0 +1,19 @@
+list = [1,2,3]
+list2 = list
+list.append(4)
+print(list)
+print(list2)
+
+
+
+x= 50
+y = x
+print(id(x))
+print(id(y))
+print(x is y)
+
+
+
+name = "Noman"
+name = "Agentic ai engineer"
+print(name)
