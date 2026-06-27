3321ade791da7812c6c2e484d340281b4377ca62
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-06-27 15:55:57 +0500

    Complete Python Foundation Day 1 Exercise

--- 01-Python-Foundations/exercises/practice day 1.py
+++ 01-Python-Foundations/exercises/practice day 1.py
@@ -8,3 +8,19 @@ print(a**b)
 
 Data = "Hello Noman, Welcome to python."
 print(Data)
+
+
+name = "Noman"
+Age = 22
+University="University of Sargodha "
+Department = "Software Engineering"
+CGPA = 3.65
+print("=========================")
+print("   Student Card   ")
+print("==========================")
+print("Name: ", name)
+print("Age: ",Age)
+print("University: ",University)
+print("Department: ",Department)
+print("CGPA: ",CGPA)
+print("===========================")
---
+++ 01-Python-Foundations/exercises/student_card.py
@@ -0,0 +1,14 @@
+name = "Noman"
+Age = 22
+University="University of Sargodha "
+Department = "Software Engineering"
+CGPA = 3.65
+print("=========================")
+print("   Student Card   ")
+print("==========================")
+print("Name: ", name)
+print("Age: ",Age)
+print("University: ",University)
+print("Department: ",Department)
+print("CGPA: ",CGPA)
+print("===========================")
