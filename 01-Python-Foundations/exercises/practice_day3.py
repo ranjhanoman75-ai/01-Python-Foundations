fb448a7063c00a02ec4310deb17755df62589f79
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-06-28 07:49:18 +0500

    Day 3 questions more completed

--- 01-Python-Foundations/exercises/practice_day3.py
+++ 01-Python-Foundations/exercises/practice_day3.py
@@ -21,3 +21,13 @@ elif num<0:
     print("The number is negative. ")
 else:
     print("The number is zero.")
+
+
+print("========program 4========")
+
+num = int(input("Enter a number: "))
+if num%2 ==0:
+    print("The number is even. ")
+else:
+    print("The number is odd. ")
+    
