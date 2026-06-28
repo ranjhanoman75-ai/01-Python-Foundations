ede66858d99c5694f24bc52a7ae15efed92eea1f
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-06-28 08:10:59 +0500

    Day 3 exercises completed

--- 01-Python-Foundations/exercises/practice_day3.py
+++ 01-Python-Foundations/exercises/practice_day3.py
@@ -30,4 +30,38 @@ if num%2 ==0:
     print("The number is even. ")
 else:
     print("The number is odd. ")
+
+
+
+print("=====program 5=======")
+num = int(input("Enter a number: "))
+if num%3==0 and num%5==0:
+    print("The number is divisible by 3 and 5.")
+else: 
+    print("The number is not divisible by 3 and 5. ")
+
+
+print("=========program 6=======")
+username = input("Enter your username: ")
+password = int(input("Enter your password: "))
+if username=="admin" and password ==12345:
+    print("Login successfull. ")
+else: 
+    print("Login failed. ")
+
+
+print("======program 7=======")
+marks = int(input("Enter your marks: "))
+if marks>= 90:
+    print("Grade A ")
+elif marks>=80:
+    print("Grade B ")
+elif marks>=70:
+    print("Grade C ")
+elif marks>=60:
+    print("Grade D ")
+elif marks<=60:
+    print("Failed")
+else: 
+    print("Invalid marks. ")
     
