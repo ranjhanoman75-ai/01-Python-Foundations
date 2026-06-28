e0417eb69d7034e78d9add2630cfbbe97d1a1ff5
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-06-28 09:43:33 +0500

    Day 4 practice started

---
+++ 01-Python-Foundations/exercises/practice_day4.py
@@ -0,0 +1,16 @@
+for i in range(5):
+    print("Hello Noman")
+
+for i in range(1,101,2):
+    print(i)
+
+
+count = 10
+while count>0:
+    print(count)
+    count-=1
+
+count = 1
+while count<11:
+    print(count)
+    count+=1
