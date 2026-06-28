c18afcf2239140b5705cf4a47887cae0807a3209
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-06-28 09:51:49 +0500

    Day 4 exercises completed

--- 01-Python-Foundations/exercises/practice_day4.py
+++ 01-Python-Foundations/exercises/practice_day4.py
@@ -14,3 +14,21 @@ count = 1
 while count<11:
     print(count)
     count+=1
+
+num = int(input("Enter a number: "))
+for i in range(1,11):
+    print(num,"*",i,"=",num*i)
+
+
+sum = 0
+for i in range(1,101):
+    sum+=i
+    print(f"The sum of first 100 numbers is: {sum}")
+
+
+num = int(input("Enter a number:"))
+for i in range(1,101):
+    if i%5==0:
+        print("The numbers that are divisible by 5",i)
+    else: 
+        print("The numbers that are not divisible by 5 ",i)
