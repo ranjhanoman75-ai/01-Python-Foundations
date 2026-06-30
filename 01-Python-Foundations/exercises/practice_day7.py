bd95edd649fd17e4afda61ce9dce92743261250d
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-06-30 07:44:56 +0500

    Reversing a list is added to day7

--- 01-Python-Foundations/exercises/practice_day7.py
+++ 01-Python-Foundations/exercises/practice_day7.py
@@ -28,3 +28,22 @@ print("=======Program 6 =========")
 data = ["Saleem",67,"Lahore"]
 data.remove("Saleem")
 print(data)
+
+print("=======Program 7 =========")
+data = ["ali",22,"Pakistani"]
+data.pop()         #remove last item
+print(data)
+
+print("=======Program 8 =========")
+data = ["Noman",22,"Jhang"]
+print(len(data))
+
+print("=======Program 9 =========")
+def reverse_list(text):
+    reverse = []
+    for i in text:
+        reverse = [i] + reverse
+    return reverse
+lists = ["Noman "," Bilal "]
+result = reverse_list(lists)
+print(result)
