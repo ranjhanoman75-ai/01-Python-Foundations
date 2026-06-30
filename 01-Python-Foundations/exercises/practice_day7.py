215becbcc1dbaebbe2028b22d07ec8d593c6e50b
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-06-30 07:15:54 +0500

    List index program printed

--- 01-Python-Foundations/exercises/practice_day7.py
+++ 01-Python-Foundations/exercises/practice_day7.py
@@ -1,3 +1,11 @@
+print("=======Program 1 =========")
+
 name = ["Ali","Bilal","Noman"]
 for item in name:
     print(item)
+
+print("=======Program 2 =========")
+items = [1,2,3,4,5]
+print(items[0])
+print(items[4])
+
