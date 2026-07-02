f615cf72a0a6e646e0167d96b9f9ef1d5169c19f
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-02 09:47:05 +0500

    Mini project added using dicitonaries

--- 01-Python-Foundations/exercises/practice_day9.py
+++ 01-Python-Foundations/exercises/practice_day9.py
@@ -46,3 +46,40 @@ student = {
     }
 }
 print(student["student2"]["name"])
+
+
+print("============Mini project ===========")
+student = {
+    "name": "Noman",
+    "Age ": 22,
+    "Course": "BSSE",
+    "RollNo": 24
+}
+
+
+while True:
+    print("1: Display student")
+    print("2: Update Course ")
+    print("3: Add Email")
+    print("4: Delete Course")
+    print("5: Exit program")
+
+    choice = int(input("Enter your choice: (1-5) "))
+    if choice == 1:
+        print(student)
+    elif choice == 2:
+        course = input("Enter the new course:")
+        student["Course"]= course
+        print("Course updated successfully! ")
+    elif choice == 3:
+        email = input("Enter the email: ")
+        student["Email"] = email
+        print("Email added successfully! ")
+    elif choice == 4:
+        student.pop("Course")
+        print("Course deleted successfully!")
+    elif choice == 5:
+        print("Thank you for using this program! ")
+        break 
+    else:
+        print("Invalid choice! Please enter choice(1-5)")
