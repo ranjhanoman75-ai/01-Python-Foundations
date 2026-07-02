8d0e1e767676262a98713864e20775633f618909
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-02 08:56:30 +0500

    questions of dictionaries with their functions

--- 01-Python-Foundations/exercises/practice_day9.py
+++ 01-Python-Foundations/exercises/practice_day9.py
@@ -10,3 +10,14 @@ student["course"]= "BSSE"
 print(student)
 student["course"]= "Software Engineering"
 print(student)
+student.pop("Age")
+print(student)
+del student["course"]
+print(student)
+print(len(student))
+if "age" in student:
+    print("found")
+else:
+    print("Not found")
+
+print(student.keys())
