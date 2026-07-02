d5ee950d1072ddc495d8121429f03be873894987
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-02 08:43:12 +0500

    Day 9 started of Dictionaries

---
+++ 01-Python-Foundations/exercises/practice_day9.py
@@ -0,0 +1,12 @@
+student = {
+    "name": "Noman",
+    "Age" : 22,
+    "city": "Jhang"
+    }
+print(student)
+print(student["name"])
+print(student["Age"])
+student["course"]= "BSSE"
+print(student)
+student["course"]= "Software Engineering"
+print(student)
