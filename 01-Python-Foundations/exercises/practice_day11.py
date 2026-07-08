c0a9da7b74520e0c68fcd4b16c35e20fe100116b
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-08 11:00:14 +0500

    Modules added day 11

---
+++ 01-Python-Foundations/exercises/practice_day11.py
@@ -0,0 +1,28 @@
+import math
+print(math.asin(1))
+print(math.acos(1))
+print(math.atan(1))
+print(math.degrees(1))
+print(math.sin(1))
+print(math.cos(1))
+print(math.tan(1))
+print(math.factorial(5))
+print(math.ceil(2.4))
+
+
+import random
+print(random.randint(1,100))
+names = ["noman","ali","bilal"]
+print(random.choice(names))
+
+from datetime import datetime
+now = datetime.now()
+print(now)
+print(now.year)
+print(now.month)
+print(now.day)
+
+import os 
+print(os.getcwd())
+print(os.listdir())
+print(os.path.exists("01-Python-Foundations/exercises/student_record.txt"))
