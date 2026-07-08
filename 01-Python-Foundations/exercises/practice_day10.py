36c08f68f536df368bdd6370ffb69c0547a0986e
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-08 08:40:40 +0500

    Zero division error

--- 01-Python-Foundations/exercises/practice_day10.py
+++ 01-Python-Foundations/exercises/practice_day10.py
@@ -3,3 +3,13 @@ if age < 18:
     raise ValueError("Age must be greater than 18 ")
 else:
     print("You are eligible to vote")
+
+try:
+    number =  int(input("Enter a number: "))
+    number2 = int(input("Enter a number: "))
+    result = number / number2
+    print("Result: ",result)
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+except ValueError:
+    print("Please enter valid numbers")
