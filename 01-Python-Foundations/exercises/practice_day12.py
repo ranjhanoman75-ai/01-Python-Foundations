321356bc6ee9e83f02ef8442720c44b9afa0cd21
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-09 07:33:42 +0500

    Arguments passed in function

--- 01-Python-Foundations/exercises/practice_day12.py
+++ 01-Python-Foundations/exercises/practice_day12.py
@@ -1,3 +1,9 @@
 def greet(name):
     print("hello",name)
 greet("Noman")
+
+def student_info(name,age,course):
+    print("Name: ",name)
+    print("Age: ",age)
+    print("Course: ",course)
+student_info("Noman",22,"Agentic Ai")
