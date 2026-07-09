9ecd55c8e1679302638b1e2610157d90974d904e
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-09 07:40:05 +0500

    Default parameter

--- 01-Python-Foundations/exercises/practice_day12.py
+++ 01-Python-Foundations/exercises/practice_day12.py
@@ -7,3 +7,17 @@ def student_info(name,age,course):
     print("Age: ",age)
     print("Course: ",course)
 student_info("Noman",22,"Agentic Ai")
+
+
+def rectangle_area(length,width):
+    return length*width
+print(rectangle_area(23,34))
+
+def info(name="Noman", age =22):
+    print(name)
+    print(age)
+info()
+
+def exponent(number,exponent=2):
+    return number*exponent
+print(exponent(3))
