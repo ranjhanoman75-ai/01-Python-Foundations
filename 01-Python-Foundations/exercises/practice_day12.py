5faca191f620823abe489ff8c07e5b8a6d859139
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-09 08:21:42 +0500

    Student grade system

--- 01-Python-Foundations/exercises/practice_day12.py
+++ 01-Python-Foundations/exercises/practice_day12.py
@@ -45,3 +45,51 @@ def add(a,b):
     """
     return a + b
 print(add.__doc__)
+
+print("=========Mini Project========")
+def input_marks():
+    marks = []
+    for i in range(5):
+        mark = float(input(f"Enter marks for Subject {i+1}: "))
+        marks.append(mark)
+    return marks
+
+
+def total_marks(marks):
+    return sum(marks)
+
+
+def percentage(total, max_marks):
+    return (total / max_marks) * 100
+
+
+def grade(percent):
+    if percent >= 90:
+        return "A+"
+    elif percent >= 80:
+        return "A"
+    elif percent >= 70:
+        return "B"
+    elif percent >= 60:
+        return "C"
+    elif percent >= 50:
+        return "D"
+    else:
+        return "F"
+
+
+def display_result():
+    marks = input_marks()
+    total = total_marks(marks)
+    max_marks = len(marks) * 100
+    percent = percentage(total, max_marks)
+    student_grade = grade(percent)
+
+    print("\n----- Result -----")
+    print("Marks:", marks)
+    print("Total Marks:", total)
+    print(f"Percentage: {percent:.2f}%")
+    print("Grade:", student_grade)
+
+
+display_result()
