e97a32350e4eed67e12e130d132a61f58bea1527
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-10 08:20:29 +0500

    *Args added

--- 01-Python-Foundations/exercises/practice_day13.py
+++ 01-Python-Foundations/exercises/practice_day13.py
@@ -2,3 +2,14 @@ print("==========Day 13 =====Advanced Functions=======")
 def add(*numbers):
     print(numbers)
 add(1,2,3,4)
+
+def add(*numbers):
+    total = 0
+    for num in numbers:
+        total += num
+
+    return total
+print(add(1,2,3))
+print(add(1,35,5,6))
+print(add(100,199))
+    
