aa3cdf729ecee74ebff1312663be3b9d2b34f2b0
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-10 09:08:52 +0500

    Local and global keyword

--- 01-Python-Foundations/exercises/practice_day13.py
+++ 01-Python-Foundations/exercises/practice_day13.py
@@ -65,6 +65,8 @@ students.sort(key=lambda x:x[1])
 
 print(students)
 
+print("======Largest number using *args======")
+
 def info(*data):
     if len(data) == 0:
         return "No data found"
@@ -77,7 +79,39 @@ def info(*data):
 print(info(1,5,6,78,32))
     
 
+def count(n):
+    if n==0:
+        return
+    print(n)
 
+    count(n-1)
 
+count(5)
 
+def factorial(n):
+    if n==1:
+        return 1
     
+    return  n* factorial(n-1)
+print(factorial(5))
+
+
+def num():
+    x=0
+    print(x)
+
+num()
+
+print("=======global variable=======")
+name = "Noman "
+def show():
+    print(name)
+
+show()
+print("=====Global Keyword=======")
+count = 0
+def show():
+    global count
+    count += 1
+show()
+print(count)
