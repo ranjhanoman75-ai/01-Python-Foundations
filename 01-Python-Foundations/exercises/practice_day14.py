6bb556339db8d984125408e84f97f0f84a02d9a9
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-11 08:39:29 +0500

    Classes with functions

--- 01-Python-Foundations/exercises/practice_day14.py
+++ 01-Python-Foundations/exercises/practice_day14.py
@@ -6,3 +6,23 @@ class student:
         print("Welcome student")
 student1 = student()
 student1.greet()
+
+class Dog:
+    def bark(self):
+        print("Dog barks")
+    def bark(self):
+        print("Dog1 barks")
+dog = Dog()
+dog.bark()
+
+dog1 = Dog()
+Dog.bark(dog1)
+
+class student:
+    def show(self):
+        print(self)
+
+std1 = student()
+std2 = student()
+std1.show()
+std2.show()
