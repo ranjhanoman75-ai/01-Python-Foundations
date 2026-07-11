0f522edf25b02f35adf0e0070e95fdbf2d34e50c
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-11 09:35:54 +0500

    Student report system added using oop version

--- 01-Python-Foundations/exercises/practice_day14.py
+++ 01-Python-Foundations/exercises/practice_day14.py
@@ -82,4 +82,53 @@ cal = calculator()
 print(cal.add(12,43))
 print(cal.subtract(144,24))
 print(cal.multiply(14,6))
-print(cal.divide(12,3))        
+print(cal.divide(12,3))       
+
+
+print("=========Student Report System===========")
+class student:
+    def input_Data(self):
+        self.name = input("Enter the student name: ")
+        self.rollno = int(input("Enter the roll no of student: "))
+        self.course = input("Enter the course of student: ")
+        marks = []
+        for i in range(5):
+            mark = float(input(f"Enter the marks of subject {i+1}: "))
+            marks.append(mark)
+        return marks
+    def calculate_Data(self, marks):
+        return sum(marks)
+    def calculate_percentage(self, total, max_marks):
+        return (total/max_marks)*100
+    def grade(self, percent):
+        if percent >= 90:
+            return "A+"
+        elif percent >= 80:
+            return "A"
+        elif percent >= 70:
+            return "B"
+        elif percent >= 60:
+            return "C"
+        elif percent >= 50:
+            return "D"
+        else:
+            return "F"
+    def display_result(self):
+        marks = self.input_Data()
+        total = self.calculate_Data(marks)
+        max_marks = 5 * 100
+        percent = self.calculate_percentage(total, max_marks)
+        grade = self.grade(percent)
+        print("\n==================Student Report======================")
+        print("Name:", self.name)
+        print("Roll No:", self.rollno)
+        print("Course:", self.course)
+        print("Marks:", marks)
+        print("Total:", total)
+        print("Percentage:", percent)
+        print("Grade:", grade)
+
+
+
+std1 = student()
+std1.display_result()
