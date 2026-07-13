3b0ff5265102dda6eca51971f605023b56cccdfb
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-13 11:15:47 +0500

    Getter function in encapsulation

--- 01-Python-Foundations/exercises/practice_day18.py
+++ 01-Python-Foundations/exercises/practice_day18.py
@@ -5,3 +5,12 @@ class animal:
         self.__salary = 50000
 an = animal()
 print(an._animal__salary)
+
+print("=========Getter===To read private data====")
+class employee:
+    def __init__(self):
+        self.__salary = 50000
+    def get_salary(self):
+        return self.__salary
+emp = employee()
+print(emp.get_salary())
