2e3a7a19a9e448350894fcd78aff066055df62d1
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-13 11:21:45 +0500

    Setter and getter in encapsulation

--- 01-Python-Foundations/exercises/practice_day18.py
+++ 01-Python-Foundations/exercises/practice_day18.py
@@ -14,3 +14,16 @@ class employee:
         return self.__salary
 emp = employee()
 print(emp.get_salary())
+
+print("====Setters======TO update the private data===")
+class employee:
+    def __init__(self):
+        self.__salary = 50000
+    def set_salary(self,salary):
+        self.__salary = salary
+    def get_salary(self):
+        return self.__salary
+emp = employee()
+emp.set_salary(80000)
+print(emp.get_salary())
+
