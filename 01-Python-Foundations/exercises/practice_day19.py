59c3a3e11a2933b1f1a605ffaa106a896aca5e32
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-14 08:15:52 +0500

    Setter using decorator

--- 01-Python-Foundations/exercises/practice_day19.py
+++ 01-Python-Foundations/exercises/practice_day19.py
@@ -1,10 +1,18 @@
 print("============Day 19================")
 print("============Decorators and Pythonic way of setters and getters=======")
+print("=====Property Decorator==========")
 class employee:
     def __init__(self):
         self.__salary = 50000
     @property
     def salary(self):
         return self.__salary
+    @salary.setter
+    def salary(self,amount):
+        if amount>0:
+            self.__salary = amount
+        else:
+            print("Invalid amount")
 emp = employee()
+emp.salary = 60000
 print(emp.salary)
