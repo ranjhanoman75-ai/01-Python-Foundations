960a6a7ac8f6916284bd4e6342a22d92fa850f63
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-16 08:48:08 +0500

    Day 20 abstract classes started

---
+++ 01-Python-Foundations/exercises/practice_day20.py
@@ -0,0 +1,14 @@
+print("=============Abstract classes=============")
+from abc import ABC, abstractmethod
+class Animal(ABC):
+    def eat(self):
+        print("Animal is eating")
+    @abstractmethod
+    def sound(self):
+        pass 
+class dog(Animal):
+    def sound(self):
+        print("Dog is barking")
+dg = dog()
+dg.eat()
+dg.sound()
