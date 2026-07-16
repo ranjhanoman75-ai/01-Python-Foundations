3100706315dc3e1cbaeec0e877aa268eccc8228a
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-16 09:02:55 +0500

    Another vehicle abstract class added

--- 01-Python-Foundations/exercises/practice_day20.py
+++ 01-Python-Foundations/exercises/practice_day20.py
@@ -12,8 +12,65 @@ class dog(Animal):
 class Cat(Animal):
     def sound(self):
         print("Cat meows")
+class Lion(Animal):
+    def sound(self):
+        print("Lion is roaring")
 dg = dog()
 dg.eat()
 dg.sound()
 cat = Cat()
+cat.eat()
 cat.sound()
+lion = Lion()
+lion.eat()
+lion.sound()
+print("==========2 program========")
+class vehicle(ABC):
+    def show_company(self):
+        print("Vehicle company")
+    @abstractmethod
+    def start(self):
+        pass
+    def stop(self):
+        pass
+    def fuel(self):
+        pass
+class Car(vehicle):
+    def start(self):
+        print("Car started")
+    def stop(self):
+        print("Car stopped")
+    def fuel(self):
+        print("Fuel: petrol")
+class Bike(vehicle):
+    def start(self):
+        print("Bike started")
+    def stop(self):
+        print("Bike stopped")
+    def fuel(self):
+        print("Fuel: petrol")
+class Truck(vehicle):
+    def start(self):
+        print("Truck started")
+    def stop(self):
+        print("Truck stopped")
+    def fuel(self):
+        print("Fuel: petrol")
+print("==========Car=======")
+car = Car()
+car.show_company()
+car.start()
+car.stop()
+car.fuel()
+print("============Bike=========")
+bike = Bike()
+bike.show_company()
+bike.start()
+bike.stop()
+bike.fuel()
+print("========Truck===========")
+truck = Truck()
+truck.show_company()
+truck.start()
+truck.stop()
+truck.fuel()
