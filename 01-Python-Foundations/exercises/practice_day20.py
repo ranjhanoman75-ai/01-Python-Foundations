d9f6136955624df11b1a28e74c23b0588f788d7c
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-16 11:38:22 +0500

    3rd program of abstract class Notification added

--- 01-Python-Foundations/exercises/practice_day20.py
+++ 01-Python-Foundations/exercises/practice_day20.py
@@ -1,4 +1,5 @@
 print("=============Abstract classes=============")
+print("========== 1 Program========")
 from abc import ABC, abstractmethod
 class Animal(ABC):
     def eat(self):
@@ -74,3 +75,49 @@ truck.show_company()
 truck.start()
 truck.stop()
 truck.fuel()
+print("========3 program===========")
+class Notification(ABC):
+    def notification_info(self):
+        print("sending notification...")
+    @abstractmethod
+    def send(self):
+        pass
+    @abstractmethod
+    def receive(self):
+        pass 
+class Email(Notification):
+    def notification_info(self):
+        print("sending email  notification.....")
+    def send(self):
+        print("Sending emial  " )
+    def receive(self):
+        print("Email received")
+class SMS(Notification):
+    def notification_info(self):
+        print("SMS notification.....")
+    def send(self):
+        print("Sending SMS")
+    def receive(self):
+        print("Sms received ")
+class Whatsapp(Notification):
+    def notification_info(self):
+        print("SMS notification.....")
+    def send(self):
+        print("Sending whatsapp message....")
+    def receive(self):
+        print("Whatsapp received. ")
+print("========Email class======")
+email = Email()
+email.notification_info()
+email.send()
+email.receive()
+print("=======SMS class========")
+sms = SMS()
+sms.notification_info()
+sms.send()
+sms.receive()
+print("========Whatsapp Class=======")
+whatsapp = Whatsapp()
+whatsapp.notification_info()
+whatsapp.send()
+whatsapp.receive()
