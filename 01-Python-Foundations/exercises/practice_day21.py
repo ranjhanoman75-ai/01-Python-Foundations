f5ddb28d8094ed5df8ed41d9f1e1c31d3f0db11b
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-17 11:35:08 +0500

    List iterator added

--- 01-Python-Foundations/exercises/practice_day21.py
+++ 01-Python-Foundations/exercises/practice_day21.py
@@ -1,4 +1,17 @@
 print("===============Iterators=================")
+print("==========With Loop==============")
 numbers = [1,23,44,43,54]
 for num in numbers:
     print(num,end=",")
+
+print("=============1 program===========")
+numbers = [23,42,524,635,64]
+iterator = iter(numbers)
+print(iterator)
+print(next(iterator))
+while True:
+    try:
+        item = next(iterator)
+        print(item)
+    except StopIteration:
+        break
