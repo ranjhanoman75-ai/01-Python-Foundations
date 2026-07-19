6546e153fddd31ac9eabf0d1ed42244254750842
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-19 09:14:33 +0500

    Login decorator added

--- 01-Python-Foundations/exercises/practice_day23.py
+++ 01-Python-Foundations/exercises/practice_day23.py
@@ -33,3 +33,16 @@ def welcome():
     print("Hello Noman")
 
 welcome()
+print("========Login Decorator=========")
+def decorator(func):
+    def act():
+        print("Checking user....")
+
+        func()
+
+        print("Access granted")
+    return act
+@decorator
+def login():
+    print("Login Successfull")
+login()
