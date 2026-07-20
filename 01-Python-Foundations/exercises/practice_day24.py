b168d5e00b52ee577b8a25421ade5b1c467fcc35
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-20 08:56:19 +0500

    Exception handling of zerodivision error

--- 01-Python-Foundations/exercises/practice_day24.py
+++ 01-Python-Foundations/exercises/practice_day24.py
@@ -12,3 +12,17 @@ class Mycontext:
 with Mycontext() as obj:
     print("Inside Block")
     obj.greet()
+
+print("===========Exception Handling=============")
+class mycontext:
+    def __enter__(self):
+        print("Entering......")
+        return self
+    def __exit__(self, exc_type, exc, tb):
+        print("leaving..........")
+
+        print("Exception type",exc_type)
+        print("Exception value",exc)
+
+with mycontext():
+    print(10/0)
