ac1967042ef64e24f774186f07f0cb00b62fea1d
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-21 08:38:59 +0500

    Logging info save to file app.log

--- 01-Python-Foundations/exercises/practice_day25.py
+++ 01-Python-Foundations/exercises/practice_day25.py
@@ -6,3 +6,13 @@ logging.info("Info")
 logging.error("Error")
 logging.warning("warning")
 logging.critical("critical error")
+
+print("=======Logging save to file=========")
+import logging 
+logging.basicConfig(
+    filename="app.log",
+    level=logging.DEBUG
+)
+logging.info("Application started")
+logging.warning("Low memory")
+logging.error("Database connection failed")
