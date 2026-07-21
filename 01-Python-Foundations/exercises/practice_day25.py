15ea0f881f45a269eaf19fdbad80454016808358
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-21 08:31:57 +0500

    Logging used to store logs info about code

---
+++ 01-Python-Foundations/exercises/practice_day25.py
@@ -0,0 +1,8 @@
+print("===========Logging Info=================")
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.debug("Debug")
+logging.info("Info")
+logging.error("Error")
+logging.warning("warning")
+logging.critical("critical error")
